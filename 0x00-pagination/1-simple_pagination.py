525aea4d48c8e327ddd02ff61f15479bed37bb8c
thattechgirlie <wanji.njoro1@gmail.com>
2024-08-18 04:27:57 -0700

    pagination tasks

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Adding code pagination
+"""
+
+import csv
+from typing import List, Tuple
+
+
+class Server:
+    """
+    Server class to paginate database
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset parameter
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Finds correct indexes to paginate dataset.
+        """
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page > 0
+        assert page_size > 0
+        csv_size = len(self.dataset())
+        start, end = index_range(page, page_size)
+        end = min(end, csv_size)
+        if start >= csv_size:
+            return []
+        return self.dataset()[start:end]
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns tuple with  a start and end index.
+    """
+    return ((page - 1) * page_size, page * page_size)
